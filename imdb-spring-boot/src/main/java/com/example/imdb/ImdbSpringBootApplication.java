d3cf51d1b6a6643b9a7447c5a868a908045e46f9
Binnur KURT <deepcloudlabs@gmail.com>
2025-11-11 11:49:14 +0300

    Update ImdbSpringBootApplication.java

--- imdb-spring-boot/src/main/java/com/example/imdb/ImdbSpringBootApplication.java
+++ imdb-spring-boot/src/main/java/com/example/imdb/ImdbSpringBootApplication.java
@@ -1,13 +1,32 @@
 package com.example.imdb;
 
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.example.imdb.entity.Movie;
+import com.example.imdb.service.MovieService;
+
 @SpringBootApplication
-public class ImdbSpringBootApplication {
+public class ImdbSpringBootApplication implements ApplicationRunner {
+	private final MovieService movieService;
+	
+	public ImdbSpringBootApplication(MovieService movieService) {
+		this.movieService = movieService;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ImdbSpringBootApplication.class, args);
+	}
+
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+		movieService.findAllMoviesByYearRange(1970, 1979)
+		            .stream()
+		            .map(Movie::getTitle)
+		            .forEach(System.out::println);
+		
 	}
 
 }
